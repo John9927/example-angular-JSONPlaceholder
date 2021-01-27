a17745e53dfaee6c00f39c35aceebbeabc907d26
Giovanni Izzi <giovanni9972@libero.it>
2021-01-27 17:01:25 +0100

    GetDataFromAPI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { JSONPlaceholderService } from './services/jsonplaceholder.service'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'example-angular-JSONPlaceholder';
+
+  data:Array<any>
+
+  constructor(private JSONPlaceholder: JSONPlaceholderService) {
+    this.data = new Array<any>()
+  }
+
+  getDataFromApi() {
+    this.JSONPlaceholder.getData().subscribe((data) => {
+      this.data = data;
+    })
+  }
+
+
 }
